bd0572c6b33cdc667c1e19e4c4703e04c82bb8fc
mudit <mudit.chhabra@knoldus.in>
2019-02-24 21:22:56 +0530

    1. Add HTML File

---
+++ target/cov/index.js
@@ -0,0 +1,5 @@
+var data = {files:[
+{"link":"calculator-7c92e8220fa75aae.4bf4b274a555ea27/index.html","title":"calculator-7c92e8220fa75aae","summary_name":"calculator-7c92e8220fa75aae","covered_class":"linePartCov","covered":"74.1","covered_lines":"20","uncovered_lines":"7","total_lines" : "27"},
+], merged_files:[]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "calculator-7c92e8220fa75aae", "date" : "2019-02-24 19:28:00", "instrumented" : 27, "covered" : 20,};
